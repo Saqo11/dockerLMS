8f84b59bddbd133d7db3bc48e1cf6195e25b49a3
Saqo11 <saqoharutyunyan19@gmail.com>
2021-07-20 10:51:49 +0400

    'asdasd'

--- src/test/java/com/lms/automation/MainTest.java
+++ src/test/java/com/lms/automation/MainTest.java
@@ -59,7 +59,7 @@ public class MainTest {
 //
       DesiredCapabilities cap = DesiredCapabilities.chrome();
 //       URL u = new URL("http://localhost:4444/wd/hub");
-           URL u = new URL("https://selenium.ci.telania.net/wd/hub");
+           URL u = new URL("https://selenium.ci.telania.net:443/wd/hub");
      RemoteWebDriver driver = new RemoteWebDriver(u,cap);
         String SITE_URL = ("https://largeaccounttest.eleapdev.com/");
         driver.get(SITE_URL);
