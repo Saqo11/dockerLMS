223c3b5868919a0b0396be8f088c2064cf2443a9
Saqo11 <saqoharutyunyan19@gmail.com>
2021-07-28 18:01:22 +0400

    'fssfdf'

--- src/test/java/com/lms/automation/LoginAdminTest.java
+++ src/test/java/com/lms/automation/LoginAdminTest.java
@@ -11,31 +11,31 @@ public class LoginAdminTest extends MainTest {
     public void dsdsd() {
         loginAdminPage.goTo();
         WebElement displayedCourse = driver.findElement(By.xpath("//h1[text()='Login into your account']"));
-       boolean courseDisplay = displayedCourse.isDisplayed();
-       assertEquals(true, courseDisplay);
+        boolean courseDisplay = displayedCourse.isDisplayed();
+        assertEquals(true, courseDisplay);
 
     }
-//    public void SingInPositiveTest() {
-//        loginAdminPage.goTo()
-//                .fillEmailField("hermineadamyan.dev@gmail.com")
-//                .fillPassField("testtest1")
-//                .clickLoginButton()
-//                .ifDisplayedText();
-//        System.out.println("All is good!!!!");
-//
-//    }
-//
-//    @Test
-//    public void SingInNegativeTest() {
-//        loginAdminPage.goTo()
-//                .fillEmailField("saqo21harustyunyan+19@gmail.com")
-//                .fillPassField("7rujds12f")
-//                .clickLoginButton()
-//                .ifDisplayedAnErrorMessage();
-//
-//
-//
-//    }
+    public void SingInPositiveTest() {
+        loginAdminPage.goTo()
+                .fillEmailField("saqo21harutyunyan+19@gmail.com")
+                .fillPassField("7rujd12f")
+                .clickLoginButton()
+                .ifDisplayedText();
+        System.out.println("All is good!!!!");
+
+    }
+
+    @Test
+    public void SingInNegativeTest() {
+        loginAdminPage.goTo()
+                .fillEmailField("saqo21harustyunyan+19@gmail.com")
+                .fillPassField("7rujds12f")
+                .clickLoginButton()
+                .ifDisplayedAnErrorMessage();
+
+
+
+    }
 
 
 }
