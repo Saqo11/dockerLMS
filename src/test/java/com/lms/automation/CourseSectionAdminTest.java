22a69304031dc2b4aacb6fcba6369f418b2418cb
Saqo11 <saqoharutyunyan19@gmail.com>
2021-07-28 10:24:15 +0400

    'asdasd'

--- src/test/java/com/lms/automation/CourseSectionAdminTest.java
+++ src/test/java/com/lms/automation/CourseSectionAdminTest.java
@@ -1,162 +1,162 @@
-package com.lms.automation;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-
-public class CourseSectionAdminTest extends MainTest {
-
-
-    @BeforeEach
-     void singIn() throws InterruptedException {
-        loginAdminPage
-                .goTo()
-                .fillEmailField("saqo21harutyunyan+19@gmail.com")
-                .fillPassField("7rujd12f")
-                .clickLoginButton()
-                .ifDisplayedText();
-        coursesSectionAdminPage
-                .goToCourses()
-                .addNewCourse();
-
-
-    }
-
-
-    @Test
-    public void searchByCourseName() {
-        coursesSectionAdminPage
-                .search_By_Course_Name()
-                .ifDisplayedAutoCurse();
-    }
-
-    @Test
-    public void searchByCourseCategoryAndCourseType() throws InterruptedException {
-        coursesSectionAdminPage
-                .search_By_Course_Category_And_Course_Type()
-                .ifDisplayedAutoCurse();
-
-    }
-
-
-    @Test
-    public void LeaveAllFieldEmptyOnCourseDefinitionSection() throws InterruptedException {
-        coursesSectionAdminPage
-                .Leave_All_Field_Empty_On_Course_Definition_Section()
-                .ifDisplayedYouDidNotFillName();
-
-    }
-
-    @Test
-    public void LessonSectionAddChapter() throws InterruptedException {
-        coursesSectionAdminPage
-                .Lesson_Section_ADDCHAPTER()
-                .ifDisplayedChapterAdd();
-
-    }
-
-    @Test
-    public void addLesson() throws InterruptedException {
-        coursesSectionAdminPage
-                .add_Lesson()
-                .ifDisplayedYouDidNotFillName();
-
-
-    }
-
-    @Test
-    public void ActivateTheLessonField() throws InterruptedException {
-        coursesSectionAdminPage
-                .Activate_The_Lesson_Field()
-                .ifDisplayedLessonSave();
-    }
-
-    @Test
-    public void LeaveEmptyContentField() throws InterruptedException {
-        coursesSectionAdminPage
-                .add_Lesson()
-                .leave_Empty_Content_field()
-                .ifDisplayedYouDidNotFillName();
-
-
-    }
-
-    @Test
-    public void addScormLesson() throws InterruptedException {
-        coursesSectionAdminPage
-                .add_Scorm_Lesson()
-                .ifDisplayedLessonActivated();
-
-
-    }
-
-    @Test
-    public void chooseLimitedCompletionTime() throws InterruptedException {
-        coursesSectionAdminPage
-                .choose_Limited_completion_time()
-                .ifDisplayedFormControl();
-
-    }
-
-    @Test
-    public void addAQuizToTheLesson() throws InterruptedException {
-        coursesSectionAdminPage
-                .add_Lesson();
-        quizResultTabs
-                .createQuizQuestion();
-        coursesSectionAdminPage
-                .add_A_Quiz_To_The_Lesson()
-                .ifDisplayedMultipleChoice();
-
-
-    }
-
-    @Test
-    public void deleteQuiz() throws InterruptedException {
-        coursesSectionAdminPage
-                .add_Lesson();
-        quizResultTabs
-                .createQuizQuestion();
-        coursesSectionAdminPage
-                .delete_Quiz()
-                .ifDisplayedQuickQuizDeleted();
-
-
-    }
-
-    @Test
-    public void editheLesson() throws InterruptedException {
-        coursesSectionAdminPage
-                .Activate_The_Lesson_Field()
-                .edit_The_Lesson()
-                .ifDisplayedYouDidNotFillName();
-
-
-    }
-
-    @Test
-    public void deleteTheLesson() throws InterruptedException {
-        coursesSectionAdminPage
-                .add_Lesson()
-                .delete_The_Lesson()
-                .ifDisplayedLessonDeleted();
-    }
-
-    @Test
-    public void the_Unassigned_Users() throws InterruptedException {
-        coursesSectionAdminPage
-                .activateCourse()
-                .the_Unassigned_Users()
-                .ifDisplayedUnassigned();
-    }
+//package com.lms.automation;
+//
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Assertions;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.support.FindBy;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+//
+//
+//public class CourseSectionAdminTest extends MainTest {
+//
+//
+//    @BeforeEach
+//     void singIn() throws InterruptedException {
+//        loginAdminPage
+//                .goTo()
+//                .fillEmailField("saqo21harutyunyan+19@gmail.com")
+//                .fillPassField("7rujd12f")
+//                .clickLoginButton()
+//                .ifDisplayedText();
+//        coursesSectionAdminPage
+//                .goToCourses()
+//                .addNewCourse();
+//
+//
+//    }
+//
+//
+//    @Test
+//    public void searchByCourseName() {
+//        coursesSectionAdminPage
+//                .search_By_Course_Name()
+//                .ifDisplayedAutoCurse();
+//    }
+//
+//    @Test
+//    public void searchByCourseCategoryAndCourseType() throws InterruptedException {
+//        coursesSectionAdminPage
+//                .search_By_Course_Category_And_Course_Type()
+//                .ifDisplayedAutoCurse();
+//
+//    }
+//
+//
+//    @Test
+//    public void LeaveAllFieldEmptyOnCourseDefinitionSection() throws InterruptedException {
+//        coursesSectionAdminPage
+//                .Leave_All_Field_Empty_On_Course_Definition_Section()
+//                .ifDisplayedYouDidNotFillName();
+//
+//    }
+//
+//    @Test
+//    public void LessonSectionAddChapter() throws InterruptedException {
+//        coursesSectionAdminPage
+//                .Lesson_Section_ADDCHAPTER()
+//                .ifDisplayedChapterAdd();
+//
+//    }
+//
+//    @Test
+//    public void addLesson() throws InterruptedException {
+//        coursesSectionAdminPage
+//                .add_Lesson()
+//                .ifDisplayedYouDidNotFillName();
+//
+//
+//    }
+//
+//    @Test
+//    public void ActivateTheLessonField() throws InterruptedException {
+//        coursesSectionAdminPage
+//                .Activate_The_Lesson_Field()
+//                .ifDisplayedLessonSave();
+//    }
+//
+//    @Test
+//    public void LeaveEmptyContentField() throws InterruptedException {
+//        coursesSectionAdminPage
+//                .add_Lesson()
+//                .leave_Empty_Content_field()
+//                .ifDisplayedYouDidNotFillName();
+//
+//
+//    }
+//
+//    @Test
+//    public void addScormLesson() throws InterruptedException {
+//        coursesSectionAdminPage
+//                .add_Scorm_Lesson()
+//                .ifDisplayedLessonActivated();
+//
+//
+//    }
+//
+//    @Test
+//    public void chooseLimitedCompletionTime() throws InterruptedException {
+//        coursesSectionAdminPage
+//                .choose_Limited_completion_time()
+//                .ifDisplayedFormControl();
+//
+//    }
+//
+//    @Test
+//    public void addAQuizToTheLesson() throws InterruptedException {
+//        coursesSectionAdminPage
+//                .add_Lesson();
+//        quizResultTabs
+//                .createQuizQuestion();
+//        coursesSectionAdminPage
+//                .add_A_Quiz_To_The_Lesson()
+//                .ifDisplayedMultipleChoice();
+//
+//
+//    }
+//
+//    @Test
+//    public void deleteQuiz() throws InterruptedException {
+//        coursesSectionAdminPage
+//                .add_Lesson();
+//        quizResultTabs
+//                .createQuizQuestion();
+//        coursesSectionAdminPage
+//                .delete_Quiz()
+//                .ifDisplayedQuickQuizDeleted();
+//
+//
+//    }
+//
+//    @Test
+//    public void editheLesson() throws InterruptedException {
+//        coursesSectionAdminPage
+//                .Activate_The_Lesson_Field()
+//                .edit_The_Lesson()
+//                .ifDisplayedYouDidNotFillName();
+//
+//
+//    }
+//
+//    @Test
+//    public void deleteTheLesson() throws InterruptedException {
+//        coursesSectionAdminPage
+//                .add_Lesson()
+//                .delete_The_Lesson()
+//                .ifDisplayedLessonDeleted();
+//    }
+//
+//    @Test
+//    public void the_Unassigned_Users() throws InterruptedException {
+//        coursesSectionAdminPage
+//                .activateCourse()
+//                .the_Unassigned_Users()
+//                .ifDisplayedUnassigned();
+//    }
 ////
 ////    @Test
 ////    public void selectTheLevelGroupFilters() throws InterruptedException {
@@ -470,17 +470,17 @@ public class CourseSectionAdminTest extends MainTest {
 //
 //    }
 
-
-    @AfterEach
-      void removeTheCourses() throws InterruptedException {
-        coursesSectionAdminPage
-                .goToCourses()
-                .search_By_Course_Name()
-                .remove_The_Courses("DELETE")
-                .ifDisplayedCourseHasBeenDeleted();
-
-    }
-
-}
+//
+//    @AfterEach
+//      void removeTheCourses() throws InterruptedException {
+//        coursesSectionAdminPage
+//                .goToCourses()
+//                .search_By_Course_Name()
+//                .remove_The_Courses("DELETE")
+//                .ifDisplayedCourseHasBeenDeleted();
+//
+//    }
+//
+//}
 
 
